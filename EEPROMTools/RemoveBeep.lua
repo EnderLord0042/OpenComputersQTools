d136bdfe5df550f7cea879ad5ae19ab9a9140a98
Quincy <55165281+EnderLord0042@users.noreply.github.com>
2020-07-04 19:18:59 -0700

    Save orphaned parenthesis

--- EEPROMTools/RemoveBeep.lua
+++ EEPROMTools/RemoveBeep.lua
@@ -9,7 +9,7 @@ end
 while string.find(EEPROM,"computer.beep%(") do
   local funcStart,_ = string.find(EEPROM,"computer.beep%(")
   local funcEnd,_ = string.find(EEPROM,")",funcStart,string.len(EEPROM))
-  EEPROM = string.sub(EEPROM, 1, funcStart) .. string.sub(EEPROM, funcEnd, string.len(EEPROM))
+  EEPROM = string.sub(EEPROM, 1, funcStart) .. string.sub(EEPROM, (funcEnd + 1), string.len(EEPROM))
 end
 
 component.eeprom.set(EEPROM)
