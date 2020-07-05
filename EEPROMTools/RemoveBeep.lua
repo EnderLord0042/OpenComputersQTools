2eeedcfa2e870600a5af96673bd2b9d80eb268f1
Quincy <55165281+EnderLord0042@users.noreply.github.com>
2020-07-04 19:03:45 -0700

    Escape my parenthesis

--- EEPROMTools/RemoveBeep.lua
+++ EEPROMTools/RemoveBeep.lua
@@ -1,14 +1,14 @@
 local component = require("component")
 local EEPROM = component.eeprom.get()
 
-if not string.find(EEPROM,"computer.beep(") then
+if not string.find(EEPROM,"computer.beep\(") then
   print("No Beeps found in current EEPROM.")
   return
 end
 
-while string.find(EEPROM,"computer.beep(") do
-  local funcStart,_ = string.find(EEPROM,"computer.beep(")
-  local funcEnd,_ = string.find(EEPROM,")",funcStart,string.len(EEPROM))
+while string.find(EEPROM,"computer.beep\(") do
+  local funcStart,_ = string.find(EEPROM,"computer.beep\(")
+  local funcEnd,_ = string.find(EEPROM,"\)",funcStart,string.len(EEPROM))
   EEPROM = string.sub(EEPROM, 1, funcStart) .. string.sub(EEPROM, funcEnd, string.len(EEPROM))
 end
 
