858def0eb110a876cbe1e47ac8182952327dc50c
Quincy <55165281+EnderLord0042@users.noreply.github.com>
2020-07-05 14:46:09 -0700

    Fix various typos

--- QTools.lua
+++ QTools.lua
@@ -7,22 +7,22 @@ local GPUProxy = component.getPrimary("gpu")
 local colorPrimary = 0x0000FF
 local colorSecondary = 0xFFFFFF
 
-if GPUProxy.getDepth() = 1 then
+if GPUProxy.getDepth() == 1 then
   colorPrimary = 0x000000 
 end
 
-local screenWidth,sceenHeight = GPUProxy.getResolution()
+local screenWidth,screenHeight = GPUProxy.getResolution()
 
 local function menu(menuName,menuEntries)
   local menuSelection = 1
-  GPUProxy.setBackground(colorPrimary))
-  GPUProxy.setForegroundground(colorSecondary)
+  GPUProxy.setBackground(colorPrimary)
+  GPUProxy.setForeground(colorSecondary)
   GPUProxy.fill(1,1,screenWidth,screenHeight," ")
   GPUProxy.set(5,4,menuName)
   GPUProxy.setBackground(colorSecondary)
   GPUProxy.fill(5,6,(screenWidth - 10),1," ")
   GPUProxy.fill(5,6,1,(screenHeight - 12)," ")
-  GPUProxy.fill(5,(screenHeight - 6),(screenWidth - 10),1," ")
+  GPUProxy.fill(5,(screenHeight - 5),(screenWidth - 10),1," ")
   GPUProxy.fill((screenWidth - 5),6,1,(screenHeight - 12)," ")
   os.sleep(500)
 end
@@ -30,7 +30,7 @@ local function desc(descName,descInfo)
   print("placeholder")
 end
 
-local testResult = menu("test",{"Option 1","Option 2", Option 3})
+local testResult = menu("Test Menu",{"Option 1","Option 2", "Option 3"})
 
 GPUProxy.setBackground(0x000000)
 GPUProxy.setForeground(0xFFFFFF)
