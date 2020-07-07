0a6533420bc01a1a519681371580a47f1f41cb1c
Quincy <55165281+EnderLord0042@users.noreply.github.com>
2020-07-07 13:56:07 -0700

    If a library only has 1 function, lua wack

--- QTools.lua
+++ QTools.lua
@@ -172,7 +172,7 @@ repeat
       menu2result = menu("EEPROM Tools",{"Remove Beep"},{{"Remove Beep Info","Many different types of BIOS have a computer.beep() function call. This removes that."}})
       if menu2result == 1 then
         if desc("Will Alter EEPROM","This will alter the EEPROM currently in your computer. Please enter the EEPROM to be altered or go back.",true) then
-          desc("Remove Beep",(require("QTools/EEPROMTools/RemoveBeep").unbeep() .. " You may now continue using QTools or exit."),false)
+          desc("Remove Beep",(require("QTools/EEPROMTools/RemoveBeep")() .. " You may now continue using QTools or exit."),false)
         end 
       end
     until(menu2result == "exit")
