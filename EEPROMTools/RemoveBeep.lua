a233ed8bc680796dd8a91cdc108be8c05b816404
Quincy <55165281+EnderLord0042@users.noreply.github.com>
2020-07-07 08:01:06 -0700

    Add a function that can be called from another

--- EEPROMTools/RemoveBeep.lua
+++ EEPROMTools/RemoveBeep.lua
@@ -1,15 +1,19 @@
 local component = require("component")
-local EEPROM = component.eeprom.get()
 
-if not string.find(EEPROM,"computer.beep%(") then
-  print("No Beeps found in current EEPROM.")
-  return
+local function = unbeep()
+  local EEPROM = component.eeprom.get()
+  
+  if not string.find(EEPROM,"computer.beep%(") then
+    return "No Beeps found in current EEPROM."
+  end
+  
+  while string.find(EEPROM,"computer.beep%(") do
+    local funcStart,_ = string.find(EEPROM,"computer.beep%(")
+    local funcEnd,_ = string.find(EEPROM,")",funcStart,string.len(EEPROM))
+    EEPROM = string.sub(EEPROM, 1, (funcStart - 1)) .. string.sub(EEPROM, (funcEnd + 1), string.len(EEPROM))
+  end
+
+  component.eeprom.set(EEPROM)
+  return "Beeps successfully removed from current EEPROM."
 end
-
-while string.find(EEPROM,"computer.beep%(") do
-  local funcStart,_ = string.find(EEPROM,"computer.beep%(")
-  local funcEnd,_ = string.find(EEPROM,")",funcStart,string.len(EEPROM))
-  EEPROM = string.sub(EEPROM, 1, (funcStart - 1)) .. string.sub(EEPROM, (funcEnd + 1), string.len(EEPROM))
-end
-
-component.eeprom.set(EEPROM)
+print(unbeep())
