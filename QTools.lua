d09ca12189b1e1aa12b17be62896830406385408
Quincy <55165281+EnderLord0042@users.noreply.github.com>
2020-07-06 21:40:18 -0700

    Fix misspelling of "Desktop"

--- QTools.lua
+++ QTools.lua
@@ -165,7 +165,7 @@ end
 
 local menu1result = nil
 repeat
-  menu1result = menu("QTools v1.0.0",{"EEPROM Tools","QNet","QDestop"},{{"EEPROM Tools Info","This is a collection of tools to allow you to easily make small changes to your EEPROM."},{"QNet Info","This is a collection of tools used in QNet, an internet system for OpenComputers based on the real Internet."},{"QDesktop Tools","This is a collection of tools for installing and modifying QDesktop, a desktop for OpenOS."}})
+  menu1result = menu("QTools v1.0.0",{"EEPROM Tools","QNet","QDesktop"},{{"EEPROM Tools Info","This is a collection of tools to allow you to easily make small changes to your EEPROM."},{"QNet Info","This is a collection of tools used in QNet, an internet system for OpenComputers based on the real Internet."},{"QDesktop Tools","This is a collection of tools for installing and modifying QDesktop, a desktop for OpenOS."}})
   if menu1result == 1 then
     local menu2result = nil
     repeat
