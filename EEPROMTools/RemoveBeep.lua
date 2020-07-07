d635f7ba17bbf4ae13d4e64505588e5bbf7cb8d3
Quincy <55165281+EnderLord0042@users.noreply.github.com>
2020-07-07 14:20:46 -0700

    Remove glitchy parenthesis

--- EEPROMTools/RemoveBeep.lua
+++ EEPROMTools/RemoveBeep.lua
@@ -1,16 +1,14 @@
-local RemoveBeep = {}
-
-local function RemoveBeep.unbeep()
+local function RemoveBeep()
   local component = require("component")
   local EEPROM = component.eeprom.get()
   
-  if not string.find(EEPROM,"computer.beep%(") then
+  if not string.find(EEPROM,"computer.beep") then
     return "No Beeps found in current EEPROM."
   end
   
-  while string.find(EEPROM,"computer.beep%(") do
-    local funcStart,_ = string.find(EEPROM,"computer.beep%(")
-    local funcEnd,_ = string.find(EEPROM,")",funcStart,string.len(EEPROM))
+  while string.find(EEPROM,"computer.beep") do
+    local funcStart,_ = string.find(EEPROM,"computer.beep")
+    local funcEnd,_ = string.find(EEPROM,"%)",funcStart,string.len(EEPROM))
     EEPROM = string.sub(EEPROM, 1, (funcStart - 1)) .. string.sub(EEPROM, (funcEnd + 1), string.len(EEPROM))
   end
 
