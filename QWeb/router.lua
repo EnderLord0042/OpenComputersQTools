76abc98149e126c9da4a180187f21f6a105fa58a
Quincy <55165281+EnderLord0042@users.noreply.github.com>
2021-01-26 09:28:01 -0800

    Create basic structure

--- QWeb/router.lua
+++ QWeb/router.lua
@@ -1,1 +1,11 @@
-e
+local component = require("component")
+local event = require("event")
+
+local hasInternet = component.isAvailable("internet")
+local hasTunnel = component.isAvailable("tunnel")
+
+local function handleModemMessage()
+  
+end
+
+event.listen("modem_message", handleModemMessage)
