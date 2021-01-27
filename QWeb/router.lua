d56df151c19322fb6a2e366360ddbbd57f0d026a
Quincy <55165281+EnderLord0042@users.noreply.github.com>
2021-01-26 20:25:32 -0800

    add findRouter response

--- QWeb/router.lua
+++ QWeb/router.lua
@@ -14,9 +14,43 @@ end
 
 local function handleModemMessage(receiverAddress, senderAddress, port, distance, header, data...)
   local packetInfo = serialization.unserialize(header)
-  local nonBreakingVersion = packetInfo["qwebVersion"]:match ".%d*.%d*"
+  local nonBreakingVersion = packetInfo["qwebVersion"]:match "%d*.%d*"
   if nonBreakingVersion == "1.0" then
-    
+    if packetInfo["packetType"] == "findRouter" then
+      local ipListFile = io.open("iplist.txt","r")
+      io.input(ipListFile)
+      local ipList = serialization.unserialize(io.read("*l"))
+      io.close(ipListFile)
+      local identityFile = io.open("identity.txt","r")
+      io.input(identityFile)
+      local identity = io.read("*l")
+      io.close(identityFile)
+      local senderIP = ""
+      for ip, address in pairs(iplist) do
+        if senderAddress == address then
+          senderIP = ip
+        end
+      end
+      if senderIP == "" then
+        local highestIP = 0
+        for ip, address in pairs(iplist) do
+          if tonumber(ip:match "%d$") >= highestIP then
+            highestIP = tonumber(ip:match "%d$") + 1
+          end
+        end
+        senderIP = identity + "." + tostring(highestIP)
+        ipList[senderIP] = senderAddress
+        ipListFile = io.open("iplist.txt","w")
+        io.output(ipListFile)
+        io.write(serialization.serialize(ipList))
+        io.close(ipListFile)
+      end
+      component.modem.send(senderAddress, port, serialization.serialize({qwebVersion="1.0.0",packetType="returnRouter",fromIP=identity,toIP=senderIP,packetPort=tostring(port)}), senderIP)
+    else if packetInfo["packetType"] == "internetRequest" then
+      
+    else if packetInfo["packetType"] == "data" then
+      
+    end
   end
 end
 
