f7750005b558a33511391d9ea49c89e95a94057c
Quincy <55165281+EnderLord0042@users.noreply.github.com>
2021-01-27 16:18:11 -0800

    hehe

--- QWeb/test.lua
+++ QWeb/test.lua
@@ -1,10 +1,11 @@
     local component = require("component")
     local event = require("event")
+    local serialization = require("serialization")
     local m = component.modem -- get primary modem component
-    m.open(123)
-    print(m.isOpen(123)) -- true
+    m.open(42)
+    print(m.isOpen(42)) -- true
     -- Send some message.
-    m.broadcast(123, "this is a test")
+    m.broadcast(42, serialization.serialize({qwebVersion="1.0.0",packetType="findRouter"}))
     -- Wait for a message from another network card.
     local _, _, from, port, _, message = event.pull("modem_message")
     print("Got a message from " .. from .. " on port " .. port .. ": " .. tostring(message))
